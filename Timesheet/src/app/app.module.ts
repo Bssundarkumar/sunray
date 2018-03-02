dd3a8cd97669c9ba62dd36f60f1e4e3bc72364fa
Sundar <bssundarkumar@gmail.com>
2018-03-01 22:23:53 -0500

    timesheet page

--- Timesheet/src/app/app.module.ts
+++ Timesheet/src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { TimesheetComponent } from './timesheet/timesheet.component';
+import { NgbdDatepickerPopupComponent } from './ngbd-datepicker-popup/ngbd-datepicker-popup.component';
 
 
 @NgModule({
@@ -13,10 +15,14 @@ import { TimesheetComponent } from './timesheet/timesheet.component';
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    TimesheetComponent
+    TimesheetComponent,
+    NgbdDatepickerPopupComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Timesheet/src/app/timesheet/timesheet.component.ts
+++ Timesheet/src/app/timesheet/timesheet.component.ts
@@ -11,5 +11,4 @@ export class TimesheetComponent implements OnInit {
 
   ngOnInit() {
   }
-
 }
